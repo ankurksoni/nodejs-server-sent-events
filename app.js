465c6fc64ee37c75c0d28163b3f182bdfd310bb6
ankurksoni <ankursoni.leonardo@gmail.com>
2023-06-25 13:07:11 +0530

    refactor: Added proper cors.

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ const cors = require('cors');
 const compression = require('compression');
 
 const app = express();
+let helmet = require("helmet");
 const PORT = 3000;
 const { SSE_HEADERS } = require('./utility/constants');
 
 let clients = [];
 let facts = [];
 
-app.use(cors());
+let corsOptions = {
+  origin: ['*'], // Compliant
+  default: "locahost"
+};
+
+app.disable("x-powered-by");
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
+app.use(helmet.hidePoweredBy());
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/health', (req, res) => res.json({ status: 'healthy' }));
 app.get('/status', (req, res) => res.json({ clients: clients.length }));
@@ -30,7 +44,7 @@ app.listen(PORT, () => {
 function eventsHandler(req, res, next) {
   res.writeHead(200, SSE_HEADERS);
   res.write(`data: CONNECTED TO SERVER FOR SSEvents\n\n`);
-  
+
   const clientId = Date.now();
   const newClient = { id: clientId, response: res };
   clients.push(newClient);
